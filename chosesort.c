d8918d93942b00d26518312fca9d4d284f4efc80
xinxin-song <83233268+xinxin-song@users.noreply.github.com>
2022-06-10 20:27:32 +0800

    Update chosesort.c

--- chosesort.c
+++ chosesort.c
@@ -3,13 +3,13 @@
 int main()
 {
 	int a[10] = {11,8,62,45,78,96,75,74,2,15};
-	for(int i = 0;i<9;i++){
+	for(int i = 0 ; i < 9 ; i++){
 		int min = 100;
-		int mark =0;
-		for(int j =10;j-i>0;j--) {
-			if(a[10-j+i]<min) {
-				mark = 10-j+i;
-				min = a[10-j+i];
+		int mark = 0;
+		for(int j = 10 ; j - i > 0 ; j--) {
+			if(a[10 - j + i] < min) {
+				mark = 10 - j + i;
+				min = a[10 - j + i];
 			}
 		}
 		int temp = a[mark];
